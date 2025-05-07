d6df7268479c3c2f38244b9252d8e06f0a69c0ef
almirakeles <almirakeless@gmail.com>
2025-05-07 21:16:16 +0300

    python variables ended

---
+++ python1.0/python_variables.py
@@ -0,0 +1,16 @@
+
+job_id = 101 
+job_title = 'Data Analyst' 
+job_salary = 125000
+company_name = "AK LTD"
+job_wfh = True
+
+print("DATA JOB DESCRIPTION")
+print("++++++++++++++++++++++")
+print("Job ID :    " , job_id)
+print("Company Name :    " , company_name)
+print("Job Tittle :    " , job_title)
+print("Salary :   ", job_salary)
+print("WFH :   " ,job_wfh)
+
+
